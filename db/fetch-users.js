5c36d5dbf3ba6b6ffd321bbfd40cd406c76a0916
TejasBhovad <tejasbhovad@gmail.com>
2024-01-23 22:28:50 +0530

    supabase: init

---
+++ db/fetch-users.js
@@ -0,0 +1,8 @@
+import { supabase } from "@/supabase";
+
+export default async function fetchUsers() {
+  const { data, error } = await supabase.from("users").select("*");
+  if (error) throw error;
+  console.log(data);
+  return data;
+}
